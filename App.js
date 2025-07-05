a8cbe3b0f6ae7aed6605611d0a1e533ee4ffcdfc
smithnojones <angeldt436@gmail.com>
2025-07-05 08:37:50 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import BottomTabNavigator from "./src/navigation/BottomTabNavigator";
+import Colors from "./src/constants/Colors";
+
+export default function App() {
+  return (</Navig);
+}
